1c03ff741b5196e73c2bb4c24d38f6fe32431522
AlecMorganNSC <Alec.Morgan@seattlecolleges.edu>
2025-07-23 12:54:05 -0700

    Added CLI prompts for user input.

--- Lab1/src/main/Main.java
+++ Lab1/src/main/Main.java
@@ -1,7 +1,19 @@
 package main;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello initial commit.");
+		System.out.println("Enter three scores to see their average score and grade!");
+		System.out.println("Please only type whole numbers between 0 and 100.");
+		System.out.println("Please don't type anything else, like letters or periods.");
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print("Please enter the first score: ");
+		int score1 = input.nextInt();
+		System.out.print("Please enter the second score: ");
+		int score2 = input.nextInt();
+		System.out.print("Please enter the third score: ");
+		int score3 = input.nextInt();
 	}
 }
