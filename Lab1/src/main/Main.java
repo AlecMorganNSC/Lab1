52f98a1256f1c7f98f9df11256bd2c9f3603da50
AlecMorganNSC <Alec.Morgan@seattlecolleges.edu>
2025-07-25 16:41:07 -0700

    Added comments to code.

--- Lab1/src/main/Main.java
+++ Lab1/src/main/Main.java
@@ -8,45 +8,55 @@ public class Main {
 	public static void main(String[] args) {
 		boolean done = false;
 		while (!done) {
+			// Try-catch in case the user enters something that can't be parsed. 
 			try {
+				// Ask the user if they want to calculate scores or grades. 
 				Scanner input = new Scanner(System.in);
 				System.out.println("Would you like to calculate test scores or bank charges?");
 				System.out.print("Enter '1' for scores or '2' for bank charges: ");
 				int choice = input.nextInt(); 
 				switch (choice) {
 					case 1: 
+						// Calculate scores if the user wants to calculate scores. 
 						scores();
+						done = true; // Stop asking the user to choose. 
 						break;
 					case 2:
+						// Calculate charges if the user wants to calculate charges. 
 						charges();
+						done = true; // Stop asking the user to choose. 
 						break;
 					default: 
 						throw new Exception();
 				}
-				done = true;
-				}
-				catch (Exception e) {
-					System.out.println("Sorry, I didn't understand that.");
-				}
+			} catch (Exception e) {
+				// Generic error message for all unhandled user inputs. 
+				System.out.println("Sorry, I didn't understand that.");
+			}
+			System.out.println(); // Visually separate the last attempt from the new one. 
 		}
 	
 	}
 	
 	public static void scores() {
-		
 		boolean done = false;
 		while (!done) {
 			try {
-				System.out.println("Enter three scores to see their average score and grade!");
+				// Give the user instructions on using the program. 
+				System.out.println();
+				System.out.println("Enter three scores to see the average score and grade!");
 				System.out.println("Please only type whole numbers between 0 and 100.");
 				System.out.println("Please don't type anything else, like letters or periods.");
 
+				// Collect scores from user. 
 				Scanner input = new Scanner(System.in);
-				int N_SCORES = 3;
+				final int N_SCORES = 3;
 				int[] scores = new int[N_SCORES];
 				for (int i = 0; i < scores.length; i ++) {
+					// "Please enter score 1, 2, 3, etc..."
 					System.out.print("Please enter score " + (i + 1) + ": ");
 					int score = input.nextInt();
+					// If score is below 0 or above 100 throw a helpfully specific error. 
 					if (score < 0) {
 						throw new Exception("That number is too small!");
 					} else if (score > 100) {
@@ -56,10 +66,11 @@ public class Main {
 					}
 				}
 		
+				// Calculate and show the average of all scores. 
 				int avg_score = IntStream.of(scores).sum() / scores.length;
-		
 				System.out.println("Average score: " + avg_score);
 		
+				// Show the corresponding average letter grade. 
 				if (avg_score >= 90) {
 					System.out.println("Average grade: A");
 				} else if (avg_score >= 80) {
@@ -71,16 +82,17 @@ public class Main {
 				} else if (avg_score >= 0) {
 					System.out.println("Average grade: F");
 				} 
-				done = true;
-			}
-			catch (Exception e) {
+				done = true; // Stop asking the user to input scores. 
+			} catch (Exception e) {
+				// Check the message attached to the Exception, if any. 
 				String error_message = e.getMessage();
 				if (error_message == null) {
+					// If we don't have a specific helpful error message say something generic. 
 					System.out.println("Sorry, I didn't understand that.");
 				} else {
+					// If we have a specific helpful error message say that. 
 					System.out.println(error_message);
 				}
-				System.out.println();
 			}	
 		}
 	}
@@ -88,14 +100,20 @@ public class Main {
 	public static void charges() {
 		boolean done = false;
 		while (!done) {
-			try {
+			// Try-catch in case the user enters something that can't be parsed. 
+			try { 
 				Scanner input = new Scanner(System.in);
+				// Give the user instructions on using the program.
+				System.out.println();
 				System.out.println("How many checks did you write in the last month?");
 				System.out.println("Please only type numbers between 0 and 999.");
 				System.out.print("Please don't type anything else, like letters or periods: ");
+				// Find out how many checks the user wrote last month. 
 				int num_checks = input.nextInt();
-				final double BASE_FEE = 10.0f;
-				float check_fee = 0.0f;
+				final double BASE_FEE = 10.0f; // base fee is always the same. 
+				// Check fee is never actually 0, but it must be prematurely initialized to avoid crashes. 
+				float check_fee = 0.0f; 
+				// Figure out the check fee. 
 				if (num_checks >= 1000) {
 					throw new Exception("That's too many checks!");
 				} else if (num_checks >= 60) {
@@ -109,20 +127,25 @@ public class Main {
 				} else if (num_checks < 0) {
 					throw new Exception("That's too few checks!");
 				}
+				// Calculate the total from the base fee and check fees. 
 				float total = (float) BASE_FEE + (check_fee * num_checks);
+				// Formatter to present numbers in localized currency style. 
 				NumberFormat formatter = NumberFormat.getCurrencyInstance();
+				// Explain all fees to the user. 
 				System.out.println("Base fee: " + formatter.format(BASE_FEE));
 				System.out.println("Check fee: " + formatter.format(check_fee));
 				System.out.println("Total service charge: " + formatter.format(total));
-				done = true; 
+				done = true; // Stop asking the user how many checks they've written. 
 			} catch (Exception e) {
+				// Check the message attached to the Exception, if any. 
 				String error_message = e.getMessage();
 				if (error_message == null) {
+					// If we don't have a specific helpful error message say something generic. 
 					System.out.println("Sorry, I didn't understand that.");
 				} else {
+					// If we have a specific helpful error message say that. 
 					System.out.println(error_message);
 				}
-				System.out.println();
 			}
 		}
 	}
